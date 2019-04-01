380cb07a7e1aaeeecf24fcd1af0baacbc6c04ce8
Giorgio Minardi <george2giga@gmail.com>
2019-04-01 17:22:26 +0100

    draft

---
+++ ApiMocker/Entities/AppSettingsSingleton.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiMocker.Entities
+{
+    public class AppSettingsSingleton
+    {
+        public int Port { get; set; }
+        public bool Https { get; set; }
+        public bool QuietLogging { get; set; }
+        public string MockFolder { get; set; }
+    }
+}
--- ApiMocker/Program.cs
+++ ApiMocker/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using ApiMocker.Entities;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace ApiMocker
@@ -22,6 +23,12 @@ namespace ApiMocker
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args)
         {
+            //var config = new ConfigurationBuilder()
+            //    .SetBasePath(Directory.GetCurrentDirectory())
+            //    .AddJsonFile("appsettings.json", optional: false)
+            //    .Build();
+
+
             var url = ApplicationSettings.Instance.Https ? "https" : "http";
             return WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
